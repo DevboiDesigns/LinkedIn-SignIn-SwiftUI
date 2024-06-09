064ede929cdc4ac14f3ac8e2fa4adcffb28eb392
DevboiDesigns <75514458+DevboiDesigns@users.noreply.github.com>
2024-06-10 02:37:44 +0700

    init project

--- Package.swift
+++ Package.swift
@@ -11,12 +11,19 @@ let package = Package(
             name: "LinkedIn-SignIn",
             targets: ["LinkedIn-SignIn"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/jdg/MBProgressHUD.git", .upToNextMajor(from: "1.2.0"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LinkedIn-SignIn"),
-        .testTarget(
+            name: "LinkedIn-SignIn",
+            dependencies: [
+                .product(name: "MBProgressHUD", package: "MBProgressHUD"),
+            ]),
+        
+            .testTarget(
             name: "LinkedIn-SignInTests",
             dependencies: ["LinkedIn-SignIn"]),
     ]
